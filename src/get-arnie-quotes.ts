5d042c48ae89c3a613f175fcbc8ed76951694b3b
lzyk9999 <lzyk9999@gmail.com>
2022-11-08 23:22:53 +1030

    done

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,31 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type SuccessfulRequest = {
+	'Arnie Quote': string;
+};
+type FailedRequest = {
+	FAILURE: string;
+};
+type TResult = SuccessfulRequest | FailedRequest;
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+	const _urls = urls.slice();
+	const results = await Promise.all(
+		_urls.map(async (url) => await getResult(url))
+	);
+	return results;
+};
+
+const getResult = async (url: string): Promise<TResult> => {
+	const { status, body } = await httpGet(url);
+	const _httpResponseBody = JSON.parse(body);
+	const _sucessfulRequest: SuccessfulRequest = { 'Arnie Quote': '' };
+	const _failedRequest: FailedRequest = { FAILURE: '' };
+	if (status === 200) {
+		_sucessfulRequest['Arnie Quote'] = _httpResponseBody.message;
+		return _sucessfulRequest;
+	} else {
+		_failedRequest.FAILURE = _httpResponseBody.message;
+		return _failedRequest;
+	}
 };
